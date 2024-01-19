46ea20afccdcca4d65d67232b649fd42cc21e691
Paul Shephard <paulshephard598@gmail.com>
2024-01-19 14:20:35 -0500

    write automated tests and successfully test user routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const userRoutes = require('./routes/userRoutes')
 const app = express()
+const bodyParser = require('body-parser')
 
 app.use(express.json())
 app.use(morgan('combined'))
